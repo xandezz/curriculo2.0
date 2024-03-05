7f99f46ca2c1a45b419adb1269d53fd1e6aea39b
Alexandre Pereira <xandedrop2@gmail.com>
2024-03-04 23:16:43 -0300

    configurações

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 //CSS
 import './App.css'
--- src/components/Carrossel.jsx
+++ src/components/Carrossel.jsx
@@ -6,7 +6,7 @@ import './Carrossel.css'
 const Carrossel = () => {
   return (
     <div className="carrossel-container">
-      <Card nome="Portifólio psicologa" descricao="Este é um market place" backgroundURL="../../public/portifoliopsicologa.jpeg" url={"https://master--psiflavialuiza.netlify.app/"} 
+      <Card nome="Portifólio psicologa" descricao="Este é um market place" backgroundURL="../../public/portifoliopsicologa.png" url={"https://master--psiflavialuiza.netlify.app/"} 
       alt={"imagem do portifolio"}/>
     </div>
   );
